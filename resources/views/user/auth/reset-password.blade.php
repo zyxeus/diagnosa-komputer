e5401ff1ebaad624e765fd5ba670cac76fa1c47d
zyxeus <ghoziagamalf01@gmail.com>
2026-01-17 00:20:06 +0700

    mengupdate form reset password

--- resources/views/user/auth/reset-password.blade.php
+++ resources/views/user/auth/reset-password.blade.php
@@ -6,7 +6,9 @@
 <link rel="apple-touch-icon" href="{{ asset('assets/img/favicon.webp') }}">
 
 <link href="{{ asset('assets/vendor/bootstrap/css/bootstrap.min.css') }}" rel="stylesheet">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
 <link href="{{ asset('assets/css/main.css') }}" rel="stylesheet">
+<link href="{{ asset('assets/css/user.css') }}" rel="stylesheet">
 </head>
 
 @extends('layouts.app')
@@ -39,12 +41,34 @@
 
             <div class="mb-3">
                 <label>Password Baru</label>
-                <input type="password" name="password" class="form-control" required>
+                <div class="input-group">
+                    <input type="password"
+                           name="password"
+                           id="new_password"
+                           class="form-control"
+                           required>
+
+                    <span class="input-group-text toggle-password"
+                          onclick="togglePassword('new_password', 'eyeNew')">
+                        <i class="fas fa-eye" id="eyeNew"></i>
+                    </span>
+                </div>
             </div>
 
             <div class="mb-3">
                 <label>Ulangi Password</label>
-                <input type="password" name="password_confirmation" class="form-control" required>
+                <div class="input-group">
+                    <input type="password"
+                           name="password_confirmation"
+                           id="confirm_password"
+                           class="form-control"
+                           required>
+
+                    <span class="input-group-text toggle-password"
+                          onclick="togglePassword('confirm_password', 'eyeConfirm')">
+                        <i class="fas fa-eye" id="eyeConfirm"></i>
+                    </span>
+                </div>
             </div>
 
             <button class="btn btn-primary w-100">
@@ -54,4 +78,22 @@
         </form>
     </div>
 </div>
+
+<script>
+function togglePassword(inputId, eyeId) {
+    const input = document.getElementById(inputId);
+    const icon = document.getElementById(eyeId);
+
+    if (input.type === 'password') {
+        input.type = 'text';
+        icon.classList.remove('fa-eye');
+        icon.classList.add('fa-eye-slash');
+    } else {
+        input.type = 'password';
+        icon.classList.remove('fa-eye-slash');
+        icon.classList.add('fa-eye');
+    }
+}
+</script>
+
 @endsection
